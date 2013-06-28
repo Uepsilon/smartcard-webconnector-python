cbb93de68bbff5e9db6e84207a0267df1ede10cb
Maxim Küpper <maxim.kuepper@gmail.com>
2013-06-28 21:58:03 +0200

    added GPIO for Raspberry IO-Pins

--- webconnector.py
+++ webconnector.py
@@ -1,17 +1,15 @@
 #! /usr/bin/env python
-
 """
 Smartcard Webconnector - Idenify with your Smartcard!
 
 Usage:
-    webconnector.py -U=<url> -E=<event-key> [-R=<resource-key>]
+    webconnector.py -U=<url> -E=<event-key>
     webconnector.py -h
     webconnector.py --version
 
 Options:
     -U --url=<url>                      Url to POST to
     -E --event-key=<event-key>          Event-Key
-    -R --resource-key=<resource-key>    Resource-Key
     -h --help                           Shows this Screen
     --version                           Shows the Version
 """
@@ -20,10 +18,12 @@ from smartcard.util import *
 from smartcard.CardType import ATRCardType
 from smartcard.CardRequest import CardRequest
 from docopt import docopt
-import urllib
+import RPi.GPIO as GPIO
+import urllib2
 import sys
 import json
 import hashlib
+import threading
 
 # define the apdus used in this script
 GET_RESPONSE = [0XA0, 0XC0, 0x00, 0x00]
@@ -40,7 +40,14 @@ class Webconnector():
     lastId = None
     url = None
     resource = None
-    event_key = None
+    eventKey = None
+
+    indicatorLeds = {
+        'success': 11,
+        'error': 13,
+        'connection': 15,
+        'power': 16
+    }
 
     """
         MainClass for the Webconnector
@@ -48,8 +55,17 @@ class Webconnector():
     def __init__(self, args):
         # set url + Resource
         self.url = args['--url']
-        self.event_key = args['--event-key']
-        self.resource = args['--resource-key'] if args['--resource-key'] else None
+        self.eventKey = args['--event-key']
+
+        # Set GPIO-Mode to BCM
+        GPIO.setmode(GPIO.BOARD)
+     
+        # Define GPIO-Output LEDs
+        for led, port in indicatorLeds.iteritems():
+            if led is 'power':
+                GPIO.setup(port, GPIO.OUT, initial=GPIO.HIGH)
+            else:
+                GPIO.setup(port, GPIO.OUT, initial=GPIO.LOW)
 
         self.cardRequest = CardRequest(timeout=None, cardType=self.cardType)
         while True:
@@ -59,8 +75,7 @@ class Webconnector():
             cardservice.connection.connect()
 
             # Get Reader-ID as Resource
-            if self.resource is None:
-                self.resource = self.getReaderID(cardservice.connection)
+            self.resource = self.getReaderID(cardservice.connection)
 
             cardUID = self.getUID(cardservice.connection)
 
@@ -80,21 +95,37 @@ class Webconnector():
     def webConnect(self, uid):
         request_data = {}
         request_data['card_uid'] = "".join(map(str, uid))
-        request_data['event_key'] = self.event_key
+        request_data['event_key'] = self.eventKey
 
         if self.resource is not None:
             request_data['resource'] = self.resource
 
         try:
             print "Calling " + str(self.url) + " with Options: " + str(request_data)
-            response = urllib.urlopen(self.url, data=urllib.urlencode(request_data))
+            response = urllib2.urlopen(self.url, data=urllib2.urlencode(request_data))
 
             if response.code is 200:
                 self.processWebResponse(response)
+                # Turn Success-LED on for 2 Sec
+                self.timedLedSwitch(indicatorLeds['success'], GPIO.HIGH, GPIO.LOW, 2)
             else:
+                # Turn Error-LED on for 2 Sec
+                self.timedLedSwitch(indicatorLeds['error'], GPIO.HIGH, GPIO.LOW, 2)
+                
                 print "Error from Webservice: " + str(response.code)
         except Exception as e:
             print e
+
+    def checkConnection(self, uid):
+        try:
+            # Try connection
+            urllib2.urlopen(self.url, timeout=1)
+
+            # Connection successful, activate Connection-LED
+            self.switchLed(self.indicatorLeds['connection'], GPIO.HIGH)
+        except urllib2.URLError as e:
+            self.switchLed(self.indicatorLeds['connection'], GPIO.LOW)
+
 
     def processWebResponse(self, response):
         if 'application/json' in response.info().getheader('Content-Type') :
@@ -143,9 +174,22 @@ class Webconnector():
             print "Card mute (or removed)"
         return response
 
+    def switchLed(self, port, status):
+        print str(port) + " will be set to " + str(status)
+        GPIO.output(port, status);
+
+    def timedLedSwitch(self, port, init, end, time):
+        # set init value
+        self.switchLed(port, init)
+
+        # create and start Timer to Call switchLed again after given Time
+        thread = threading.Timer(time, switchLed, [port, end]).start()
+
+
 
 if __name__ == '__main__':
-    webconnector = None
-
-    args = docopt(__doc__, version='Smartcard Webconnector: 0.1')
-    webconnector = Webconnector(args)
+    try: 
+        args = docopt(__doc__, version='Smartcard Webconnector: 0.1')
+        webconnector = Webconnector(args)
+    finally:
+        GPIO.cleanup()
