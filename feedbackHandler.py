70998012668423fb355400ea0c59735afe020305
Maxim Küpper <maxim.kuepper@gmail.com>
2013-06-30 23:12:29 +0200

    reverted changed from raspberry branch

--- feedbackHandler.py
+++ feedbackHandler.py
@@ -1,34 +1,20 @@
-import RPi.GPIO as GPIO
+SUCCESS = "Success"
+POWER = "Power"
+ERROR = "Error"
+CONNECTION = "Connection"
 
-SUCCESS = 15
-POWER = 13
-ERROR = 11
-CONNECTION = 16
-
-ACTIVE = GPIO.HIGH
-INACTIVE = GPIO.LOW
+ACTIVE = "Active"
+INACTIVE = "Inactive"
 
 def setup():
-    # Set GPIO-Mode to BCM
-    GPIO.setmode(GPIO.BOARD)
-
-    # Define GPIO-Output LEDs
-    GPIO.setup(POWER, GPIO.OUT, initial=GPIO.HIGH) # Power-Indikator
-
-    GPIO.setup(SUCCESS, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(ERROR, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(CONNECTION, GPIO.OUT, initial=GPIO.LOW)
+    setFeedback(POWER, ACTIVE)
 
 def setFeedback(channel, status):
-    GPIO.output(channel, status);
+    print "Set Channel '" + str(channel) + "' to '" + str(status) + "'"
 
 def shutdown():
-    # Turn all LEDs off before leaving
-    GPIO.output(POWER, INACTIVE)
-    GPIO.output(SUCCESS, INACTIVE)
-    GPIO.output(ERROR, INACTIVE)
-    GPIO.output(CONNECTION, INACTIVE)
-
-    # and free GPIO-Interface
-    GPIO.cleanup()
+    setFeedback(SUCCESS, INACTIVE)
+    setFeedback(POWER, INACTIVE)
+    setFeedback(ERROR, INACTIVE)
+    setFeedback(CONNECTION, INACTIVE)
 
